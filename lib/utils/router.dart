4ccbe8c31374cca56cc58f0c980fb9bdaa3bd8ad
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-06-29 23:59:21 +0100

    Registration Provider logic

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,25 +1,25 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-// //Utils folders are used to store  functions that are used in multiple places.
+//Utils folders are used to store  functions that are used in multiple places.
 
-// //PageNavigator is a class used to navigate to different pages in the app.
+//PageNavigator is a class used to navigate to different pages in the app.
 
-// class PageNavigator {
-//   //PageNavigator variable and  Constructor
-//   BuildContext? ctx;
+class PageNavigator {
+  //PageNavigator variable and  Constructor
+  BuildContext? ctx;
 
-//   PageNavigator({this.ctx});
+  PageNavigator({this.ctx});
 
-//   //Navigate to next page using Cupertino page Route
-//   Future nextPage(Widget? page) {
-//     return Navigator.push(
-//         ctx!, CupertinoPageRoute(builder: (context) => page!));
-//   }
+  //Navigate to next page using Cupertino page Route
+  Future nextPage(Widget? page) {
+    return Navigator.push(
+        ctx!, CupertinoPageRoute(builder: (context) => page!));
+  }
 
-//   //Navigate to the next page and removee the previous from the stack
-//   void nextPageOnly(Widget? page) {
-//     Navigator.pushAndRemoveUntil(
-//         ctx!, MaterialPageRoute(builder: (context) => page!), (route) => false);
-//   }
-// }
+  //Navigate to the next page and removee the previous from the stack
+  void nextPageOnly(Widget? page) {
+    Navigator.pushAndRemoveUntil(
+        ctx!, MaterialPageRoute(builder: (context) => page!), (route) => false);
+  }
+}
