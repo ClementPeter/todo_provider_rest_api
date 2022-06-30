e8164a480eb7a65d4ec9795de5a6bf54b77611b6
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-06-30 23:52:40 +0100

    Local Database Storage

--- lib/utils/snack_message.dart
+++ lib/utils/snack_message.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/material.dart';
-import 'package:todo_provider_rest_api/styles/colors.dart';
+// import 'package:flutter/material.dart';
+// import 'package:todo_provider_rest_api/styles/colors.dart';
 
-//Display snackbar / toast message on the app screen
-void showMessage({String? message, BuildContext? context}) {
-  ScaffoldMessenger.of(context!).showSnackBar(
-    SnackBar(
-      content: Text(
-        message!,
-        style: TextStyle(color: white),
-      ),
-      backgroundColor: primaryColor,
-    ),
-  );
-}
+// //Display snackbar / toast message on the app screen
+// void showMessage({String? message, BuildContext? context}) {
+//   ScaffoldMessenger.of(context!).showSnackBar(
+//     SnackBar(
+//       content: Text(
+//         message!,
+//         style: TextStyle(color: white),
+//       ),
+//       backgroundColor: primaryColor,
+//     ),
+//   );
+// }
