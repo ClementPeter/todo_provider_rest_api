b2906f4db747d58166a9e765bb316073d5535300
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-06-30 23:53:06 +0100

    Local Database Storage

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -1,19 +1,19 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// //Our App Color style is referenced from here
+//Our App Color style is referenced from here
 
-// Color primaryColor = const Color(0Xff742DDD);
+Color primaryColor = const Color(0Xff742DDD);
 
-// Color lightAmber = const Color(0xffFFF9E7);
+Color lightAmber = const Color(0xffFFF9E7);
 
-// Color amber = const Color(0XffEDB200);
+Color amber = const Color(0XffEDB200);
 
-// Color green = const Color(0Xff009120);
+Color green = const Color(0Xff009120);
 
-// Color grey = const Color(0Xff777777);
+Color grey = const Color(0Xff777777);
 
-// Color lightGrey = const Color(0XffE9EAE5);
+Color lightGrey = const Color(0XffE9EAE5);
 
-// Color white = const Color(0XffFFFFFF);
+Color white = const Color(0XffFFFFFF);
 
-// Color black = const Color(0Xff06051B);
+Color black = const Color(0Xff06051B);
